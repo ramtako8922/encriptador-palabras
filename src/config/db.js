b577766c2bfebb76db31a966eb023b3debdb9343
Ricardo <rmc892020@gmail.com>
2023-05-07 19:56:55 -0500

    nuevo proyecto

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mysql=require('mysql2')
-const dotenv=require('dotenv')
-
-dotenv.config();
-
-const conection=mysql.createConnection(process.env.DATABASE_URL);
-
-export default conection.primise();

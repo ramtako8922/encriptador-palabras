2f7683dba7e2b5389ca3f4d2186131df63d9aa9b
Ricardo <rmc892020@gmail.com>
2023-05-11 03:47:03 -0500

    terminado lógica de encriptador

---
+++ js/main.js
@@ -0,0 +1,168 @@
+//Constantes  que toman elementos del dom
+ const word=document.getElementById("word");
+ const word2=document.getElementById("word2");
+ const draw=document.getElementById("draw");
+ const contenido=document.getElementById("output1");
+ const contenido2=document.getElementById("output2");
+ 
+ 
+//Funcion que incrypta la palabra ingresada por el usuario de acuerdo a una matriz que contine por cual cadadena se rempleza la vocal de la palabra
+ function Encrypter(StringEncryptado) {
+    let matriz=[["e","enter"],["i","ines"],["a","ai"],["o","ober"],["u","ufat"]];
+    StringEncryptado=StringEncryptado.toLowerCase();
+ 
+    for (let index = 0; index < matriz.length; index++) {
+     if (StringEncryptado.includes(matriz[index][0])) {
+ 
+         StringEncryptado=StringEncryptado.replaceAll(matriz[index][0],matriz[index][1]);
+         
+     }
+ 
+     
+     
+    }
+         return StringEncryptado;
+ 
+  }
+
+//Funcion que desencrypta la palabra ingresada por el usuario de acuerdo a una matriz que contiene por cual vocal se rempleza la cadena de la palabra
+  function Decrypter(StringDecrypted) {
+    let matriz=[["e","enter"],["i","ines"],["a","ai"],["o","ober"],["u","ufat"]];
+    StringDecrypted=StringDecrypted.toLowerCase();
+ 
+    for (let index = 0; index < matriz.length; index++) {
+     if (StringDecrypted.includes(matriz[index][1])) {
+ 
+      StringDecrypted=StringDecrypted.replaceAll(matriz[index][1],matriz[index][0]);
+         
+     }
+ 
+     
+     
+    }
+         return StringDecrypted;
+ 
+  }
+   //Funcion que copia el texto encriptado o desencriptado en el portapaeles
+  function CopyText(text) {
+    let copy=document.getElementById("copy");
+    text=word2;
+    navigator.clipboard.writeText(word2.value);
+    console.log(word2.textContent)
+    copy.textContent="copiado"
+
+    
+  }
+
+  //Fucion que muestra el texto encriptado en la interfaz
+
+ function ShowOutput() {
+ draw.style.display="none";
+ contenido.style.display="none";
+ contenido2.style.visibility="visible";
+ const textEncrypted=Encrypter(word.value);
+ word2.value=textEncrypted;
+ word.value=""
+
+    
+ } 
+
+  //Fucion que muestra el texto desencriptado en la interfaz
+
+ function ShowOutput2() {
+
+  draw.style.display="none";
+  contenido.style.display="none";
+  contenido2.style.visibility="visible";
+  const textDecrypted=Decrypter(word.value);
+  word2.value=textDecrypted;
+  word.value=""
+ 
+     
+  } 
+
+  //Fucion que valida el texto antes de encriptarlo
+
+function SendText(){
+     let warning=""
+     let regexPalabra=/^[a-zA-Z\u00C0-\u017F]+$/
+     let enter=false;
+     document.getElementById("clear").innerHTML=""
+    
+
+    if (word.value=="") {
+    warning+=`el texto no puede quedar vacio <br>`
+    enter=true
+   
+    } 
+
+    if (!regexPalabra.test(word.value!="")) {
+       
+     warning+=`el texto no puede tener tildes <br>`
+     enter=true
+        
+ }
+
+    if(enter){
+      document.getElementById("clear").innerHTML=warning;
+
+     }else{
+        document.getElementById("clear").innerHTML="enviado";
+
+        ShowOutput();
+
+    }
+  }
+     //Fucion que valida el texto antes de desencriptarlo
+    
+    function SendText2(){
+       let warning=""
+       let regexPalabra=/^[a-zA-Z\u00C0-\u017F]+$/
+       let enter=false;
+       document.getElementById("clear").innerHTML=""
+      
+  
+      if (word.value=="") {
+      warning+=`el texto no puede quedar vacio <br>`
+      enter=true
+     
+      } 
+  
+      if (!regexPalabra.test(word.value!="")) {
+         
+       warning+=`el texto no puede tener tildes <br>`
+       enter=true
+          
+   }
+  
+      if(enter){
+        document.getElementById("clear").innerHTML=warning;
+  
+       }else{
+          document.getElementById("clear").innerHTML="enviado";
+  
+          ShowOutput2();
+  
+      }
+  
+      
+  
+
+  
+  }
+   
+
+
+
+
+  
+
+   
+       
+     
+
+        
+
+
+
+
